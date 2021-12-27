f83dc03bd0d5e102cce01ed4ef1076ac2ca30f3a
Zsolt <mozb17@gmail.com>
2021-12-27 21:26:54 +0100

    11. fejezet

---
+++ HiddenVilla_Client/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "succes", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+        public static async ValueTask SwalSuccess(this IJSRuntime jSRuntime, string message, string submessage)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowSwal", "succes", message, submessage);
+        }
+
+        public static async ValueTask SwalError(this IJSRuntime jSRuntime, string message, string submessage)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowSwal", "error", message, submessage);
+        }
+
+    }
+}
---
+++ HiddenVilla_Client/wwwroot/js/Common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "succes") {
+        toastr.success(message, 'Operation successful')
+    }
+    if (type === "error") {
+        toastr.error(message, 'Operation failed')
+    }
+}
